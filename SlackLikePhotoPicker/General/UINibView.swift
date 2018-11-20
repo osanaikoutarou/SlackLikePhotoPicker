e21f3960696b69b4eac1b0016695263f663ccd2c
osanai <osanai@CR-040.local>
2018-11-20 12:18:32 +0900

    アルバムから取得しサムネイルを表示するs

---
+++ SlackLikePhotoPicker/General/UINibView.swift
@@ -0,0 +1,21 @@
+//
+//  UINibView.swift
+//  SlackLikePhotoPicker
+//
+//  Created by osanai on 2018/11/19.
+//  Copyright © 2018年 osanai. All rights reserved.
+//
+
+import UIKit
+
+class UINibView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SlackLikePhotoPicker/InputView/CustomInputView.swift
@@ -0,0 +1,13 @@
+//
+//  CustomInputView.swift
+//  SlackLikePhotoPicker
+//
+//  Created by osanai on 2018/11/19.
+//  Copyright © 2018年 osanai. All rights reserved.
+//
+
+import UIKit
+
+class CustomInputView: CustomInputView {
+
+}
